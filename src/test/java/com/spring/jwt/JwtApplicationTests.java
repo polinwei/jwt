18bd0932ec83a14231b476de16661acdea309aa7
polin.wei <polin.wei@gmail.com>
2018-07-13 18:11:15 +0800

    Model 要由 FetchType.LAZY 改為 FetchType.EAGER 才會運作正常. 目前找不到解決方法. application.properties 加入參數也沒有用: spring.jpa.open-in-view=true spring.jpa.properties.hibernate.enable_lazy_load_no_trans=true

--- src/test/java/com/spring/jwt/JwtApplicationTests.java
+++ src/test/java/com/spring/jwt/JwtApplicationTests.java
@@ -1,16 +1,79 @@
 package com.spring.jwt;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.UserRequestPostProcessor;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.spring.jwt.authentication.security.JwtUser;
+import com.spring.jwt.authentication.security.JwtUserFactory;
+import com.spring.jwt.db.maria.dao.authentication.UserRepository;
+import com.spring.jwt.db.maria.dao.demo.BankAccountDao;
+import com.spring.jwt.db.maria.model.authentication.User;
+import com.spring.jwt.db.maria.model.demo.BankAccount;
+import com.spring.jwt.db.maria.service.demo.BankAccountService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class JwtApplicationTests {
+	
+	private final static Logger logger = LoggerFactory.getLogger(JwtApplicationTests.class);
 
+	@Autowired
+	private BankAccountService bankAccountService;
+	@Autowired
+	private BankAccountDao bankAccountDao;
+	@Autowired
+	private UserRepository userRepository;
+	
 	@Test
 	public void contextLoads() {
 	}
+	
+	
+	/**
+	 * Delete all myspring.bank_account
+	 * @throws Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		//bankAccountDao.deleteAll();
+	}
+	
+	/**
+	 * step 1: insert three records into myspring.bank_account
+	 * step 2: verify whether has three records in table: bank_account
+	 */
+	@Test
+	public void testInsertToBankAccount() {
+		BankAccount bk = new BankAccount(1L, "Tom", 1000);
+		bankAccountDao.save(bk);
+		bk = new BankAccount(2L, "Jerry", 2000);
+		bankAccountDao.save(bk);
+		bk = new BankAccount(3L, "Donald", 3000);
+		bankAccountDao.save(bk);
+
+		for (BankAccount bankAccount: bankAccountDao.findAll() ) {
+			logger.info(bankAccount.toString());
+		}
+
+		Assert.assertEquals(3,bankAccountDao.count());
+	}
+	
+	@Test
+	public void testUserAuthority() {
+		User user = userRepository.findById(1L).get();
+		JwtUser jwtUser = JwtUserFactory.create(user);
+		logger.info(jwtUser.getUsername());
+		
+	}
+	
+	
 
 }
