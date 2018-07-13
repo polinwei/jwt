be52e9ad04ccc126eb6eb35b0454c25a8686beec
polin.wei <polin.wei@gmail.com>
2018-07-13 18:12:40 +0800

    Model 要由 FetchType.LAZY 改為 FetchType.EAGER 才會運作正常. 目前找不到解決方法.
    application.properties 加入參數也沒有用:
    spring.jpa.open-in-view=true
    spring.jpa.properties.hibernate.enable_lazy_load_no_trans=true

---
+++ src/main/java/com/spring/jwt/authentication/security/JwtUserFactory.java
@@ -0,0 +1,56 @@
+package com.spring.jwt.authentication.security;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.assertj.core.util.Lists;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.spring.jwt.db.maria.model.authentication.Authority;
+import com.spring.jwt.db.maria.model.authentication.User;
+import com.spring.jwt.db.maria.model.authentication.UserAuthority;
+import com.spring.jwt.exception.CommonTransactionException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public final class JwtUserFactory {
+
+	private final static Logger logger = LoggerFactory.getLogger(JwtUserFactory.class);
+    private JwtUserFactory() {
+    }
+
+    @Transactional(propagation=Propagation.REQUIRED, readOnly=true, noRollbackFor=Exception.class)
+    public static JwtUser create(User user) {
+        return new JwtUser(
+                user.getId(),
+                user.getUsername(),
+                user.getFirstname(),
+                user.getLastname(),
+                user.getEmail(),
+                user.getPassword(),
+                mapToGrantedAuthorities(user.getUserAuthorities()),
+                user.getEnabled(),
+                user.getLastpasswordresetdate()
+        );
+    }
+
+    private static List<GrantedAuthority> mapToGrantedAuthorities(Set<UserAuthority> authorities) {
+    	List<GrantedAuthority> grantedAuthority = new ArrayList<>();
+    	
+    	for (UserAuthority userAuthority : authorities) {
+    		logger.info(userAuthority.getAuthority().getName());
+    		grantedAuthority.add(new SimpleGrantedAuthority(userAuthority.getAuthority().getName()));
+    		
+		}
+    	
+        return authorities.stream()
+                .map(UserAuthority -> new SimpleGrantedAuthority(UserAuthority.getAuthority().getName()))
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/com/spring/jwt/db/maria/dao/authentication/UserRepository.java
@@ -0,0 +1,9 @@
+package com.spring.jwt.db.maria.dao.authentication;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.spring.jwt.db.maria.model.authentication.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
